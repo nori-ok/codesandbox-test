9b06f14aaa38b886bef87b8ce1962b48ac6cf9e3
nori-ok <91051495+nori-ok@users.noreply.github.com>
2021-09-23 11:46:43 +0900

    分割代入

--- src/index.js
+++ src/index.js
@@ -49,14 +49,57 @@
 /**
  * テンプレート文字列
  */
-const name = "じゃけぇ";
-const age = 29;
-//私の名前はじゃけぇです。年齢は28歳です。
+// const name = "じゃけぇ";
+// const age = 29;
+// //私の名前はじゃけぇです。年齢は29歳です。
 
-// 従来の方法
-const message1 = "私の名前は" + name + "です。年齢は" + age + "です。";
-console.log(message1);
+// // 従来の方法
+// const message1 = "私の名前は" + name + "です。年齢は" + age + "です。";
+// console.log(message1);
 
-// テンプレート文字列を用いた方法
-const message2 = `私の名前は${name}です。年齢は${age}です。`;
-console.log(message2);
+// // テンプレート文字列を用いた方法
+// const message2 = `私の名前は${name}です。年齢は${age}です。`;
+// console.log(message2);
+
+/**
+ * アロー関数
+ */
+//従来の関数
+// const func1 = function (str) {
+//   return str;
+// };
+// console.log(func1("funcです"));
+
+// const func2 = (str) => {
+//   return str;
+// };
+// console.log(func2("func2です"));
+
+// const func3 = (x, y) => {
+//   return x + y;
+// };
+// console.log(func3(2, 3));
+
+/**
+ * 分割代入
+ */
+// const myProfile = {
+//   name: "じゃけぇ",
+//   age: 28
+// };
+
+// const message1 = `名前は${myProfile.name}です。年齢は${myProfile.age}歳です。`;
+// console.log(message1);
+
+// const { name, age } = myProfile;
+// const message2 = `名前は${name}です。年齢は${age}歳です。`;
+// console.log(message2);
+
+const myProfile = ["じゃけぇ", 28];
+
+const message3 = `名前は${myProfile[0]}です。年齢は${myProfile[1]}歳です`;
+console.log(message3);
+
+const [name, age] = myProfile;
+const message4 = `名前は${myProfile[0]}です。年齢は${myProfile[1]}歳です`;
+console.log(message4);
